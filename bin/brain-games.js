374bf421abde2cf289dab294ac264b29539074b9
boss <bartellbe@gmail.com>
2025-10-05 15:54:36 +0000

    Add eslint Fix style

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,3 @@ import greetings from '../src/cli.js'
 
 console.log('Welcome to the Brain Games!')
 greetings()
-
--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync'
 const greetings = () => {
   const name = readlineSync.question('May I have yout name? ')
   console.log(`Hello, ${name}!`)
-};
+}
 
-export default greetings;
+export default greetings
